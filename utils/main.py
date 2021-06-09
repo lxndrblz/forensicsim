ed80f881f809ee746123f3c6fcee4395939e6e0d
Alexander Bilz <mail@alexbilz.com>
2021-06-09 16:46:20 +0100

    Restructured nested schema handling for increased flexibility

--- utils/main.py
+++ utils/main.py
@@ -43,6 +43,7 @@ def strip_html_tags(value):
         text = text.replace('\x00', '')
         text = text.replace('\x01', '')
         text = text.replace('\x02', '')
+        text = text.replace('\x03', '')
         text = text.replace('\xa0', '')
         return text
     except:
@@ -71,11 +72,11 @@ def parse_db(filepath):
     parse_records(fetched_ldb_records)
 
 
-def get_nested_data_structures(record):
-    nested_schemas = record.value.split(b'[{')[-1:]
+def get_nested_data_structures(record, schema):
+    nested_schemas = record.value.split(b'[{'+schema)[-1:]
     nested_schemas = nested_schemas[0].split(b'}]')[:-1]
     # Add search criteria back to the string to make list and dictionary structures complete again
-    byte_str = b'[{' + nested_schemas[0] + b'}]'
+    byte_str = b'[{'+schema + nested_schemas[0] + b'}]'
     # turn the byte string into a Python list with dictionaries
     nested_dictionary = ast.literal_eval(byte_str.decode('utf-8'))
     return nested_dictionary
@@ -97,45 +98,49 @@ def get_content(record):
         return content_utf8_encoded[2::]
 
 def determine_record_type(record):
-    types = {
+    message_types = {
         'reaction_in_chat': {'identifier': {b'activityType': 'reactionInChat'},
                              'fields': [b'activityType', b'messagetype', b'contenttype', b'activitySubtype',
-                                        b'activityTimestamp', b'composetime', b'sourceUserImDisplayName'],
-                             'nested_schema': None},
-        'media': {'identifier': {b'messagetype': 'Text'},
-                  'fields': [b'messagetype', b'imdisplayname', b'composetime', b'files'], 'nested_schema': b'files'},
+                                        b'activityTimestamp', b'composetime', b'sourceUserImDisplayName']},
+        'plain': {'identifier': {b'messagetype': 'Text'},
+                  'fields': [b'messagetype', b'imdisplayname', b'composetime']},
         'message': {'identifier': {b'messagetype': 'RichText/Html'},
-                    'fields': [b'messagetype', b'contenttype', b'imdisplayname', b'clientmessageid', b'composetime', b'originalarrivaltime', b'clientArrivalTime'],
-                    'nested_schema': None},
+                    'fields': [b'messagetype', b'contenttype', b'imdisplayname', b'clientmessageid', b'composetime', b'originalarrivaltime', b'clientArrivalTime']},
         'call': {'identifier': {b'messagetype': 'Event/Call'},
-                 'fields': [b'messagetype', b'displayName', b'originalarrivaltime', b'clientArrivalTime'],
-                 'nested_schema': None},
+                 'fields': [b'messagetype', b'displayName', b'originalarrivaltime', b'clientArrivalTime']}
 
     }
+    # Lets identify nested schemas based the the schema type
+    # TODO implement Hyplinks Type
+    nested_schema = {
+        # 'hyperlinks': {'identifier': b'"@type":"http://schema.skype.com/HyperLink"'},
+        'files': {'identifier': b'"@type":"http://schema.skype.com/File"'}
+    }
 
-    for key in types:
+    for key in message_types:
         if record.value.find(b'"') != -1:
             t = True
             cleaned_record = {}
             key_values = record.value.split(b'"')
             for i, field in enumerate(key_values):
                 # check if field is a key - ignore the first byte as it is usually junk
-                if field[1::] in types[key]['fields']:
+                if field[1::] in message_types[key]['fields']:
                     # use current field as key, use next field as value
                     cleaned_record[field[1::]] = strip_html_tags(decode_value(key_values[i + 1][1::]))
-                # Get nested schemas, such as files
-                if field[1::] == types[key]['nested_schema']:
-                    nested = get_nested_data_structures(record)
-                    cleaned_record[field[1::]] = nested
+            # Get nested schemas, such as files or hyperlinks, could be both
+            cleaned_record[b'nested_content'] = []
+            for schema in nested_schema:
+                if nested_schema[schema]['identifier'] in record.value:
+                    cleaned_record[b'nested_content'].append(get_nested_data_structures(record, nested_schema[schema]['identifier']))
 
             # Determine the message type by checking if the identifiers match
-            for identifier_key in types[key]['identifier']:
+            for identifier_key in message_types[key]['identifier']:
                 if identifier_key in cleaned_record:
-                    if cleaned_record[identifier_key] != types[key]['identifier'][identifier_key]:
+                    if cleaned_record[identifier_key] != message_types[key]['identifier'][identifier_key]:
                         t = False
 
             # Lets only consider the entries that are complete and that have a valid content type
-            if t and all(c in cleaned_record for c in types[key]['fields']):
+            if t and all(c in cleaned_record for c in message_types[key]['fields']):
                 cleaned_record[b'type'] = key
                 if key == 'message':
                     # Patch the content of messages by specifically looking for divs
@@ -162,10 +167,11 @@ def parse_records(fetched_ldb_records):
             cleaned_record["state"] = fetched_record.state.name
             cleaned_record["was_compressed"] = fetched_record.was_compressed
             cleaned_records.append(cleaned_record)
+            print(cleaned_record)
 
     # Filter by messages
-    messages = [d for d in cleaned_records if d['type'] == 'message']
-    parse_text_message(messages)
+    #messages = [d for d in cleaned_records if d['type'] == 'message']
+    #parse_text_message(messages)
 
     # Filter by reactions
     # reactions = [d for d in cleaned_records if d['type'] == 'reaction_in_chat']
