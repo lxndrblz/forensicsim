9f37fedbb610d6dc8844184b7498e053f9f0019b
Alexander Bilz <mail@alexbilz.com>
2021-06-15 17:50:07 +0100

    Added functionality for clearing the app data temp directory

--- build.bat
+++ build.bat
@@ -3,6 +3,8 @@ pyinstaller --noconfirm --onefile --console --add-data "C:/Users/Alexander Bilz/
 :: Copy the two files of interest into the Autopsy plugin directory - overwrite if necessary
 xcopy /y "C:\dev\forensicsim\dist\main.exe" "C:\Users\Alexander Bilz\AppData\Roaming\autopsy\python_modules\forensicsim"
 xcopy /y "C:\dev\forensicsim\Forensicsim_Parser.py" "C:\Users\Alexander Bilz\AppData\Roaming\autopsy\python_modules\forensicsim"
+:: Remove the temp directory
+IF EXIST "C:\Users\ALEXAN~1\AppData\Local\Temp\Autopsy\Case\teams_plugin_20210615_141426\1\testdata\IndexedDB\https_teams.microsoft.com_0.indexeddb.leveldb" RMDIR /s/q "C:\Users\ALEXAN~1\AppData\Local\Temp\Autopsy\Case\teams_plugin_20210615_141426\1\testdata\IndexedDB\https_teams.microsoft.com_0.indexeddb.leveldb"
 
 
 
