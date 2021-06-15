19ca9ed57a53847a48d23dff483a211be07dd3fb
Alexander Bilz <mail@alexbilz.com>
2021-06-15 17:49:35 +0100

    Initial successful import of text messages

--- Forensicsim_Parser.py
+++ Forensicsim_Parser.py
@@ -36,8 +36,9 @@ import csv
 import inspect
 import json
 import os
-from calendar import calendar
+import calendar
 from datetime import datetime
+
 from subprocess import Popen, PIPE
 
 from java.io import File
@@ -60,6 +61,7 @@ from org.sleuthkit.autopsy.coreutils import Logger
 from org.sleuthkit.autopsy.coreutils import ExecUtil
 from org.sleuthkit.autopsy.datamodel import ContentUtils
 from org.sleuthkit.autopsy.casemodule import Case
+from org.sleuthkit.datamodel import CommunicationsManager
 
 
 # Factory that defines the name and details of the module and allows Autopsy
@@ -78,7 +80,7 @@ class ForensicIMIngestModuleFactory(IngestModuleFactoryAdapter):
         return "Data Source-level Ingest Module for parsing Microsoft Team's LevelDb databases."
 
     def getModuleVersionNumber(self):
-        return "1.0"
+        return "0.0.1"
 
     def isDataSourceIngestModuleFactory(self):
         return True
@@ -95,20 +97,24 @@ class ForensicIMIngestModule(DataSourceIngestModule):
         self._logger.logp(level, self.__class__.__name__, inspect.stack()[1][3], msg)
         self._logger = Logger.getLogger(self.__class__.__name__)
 
-    # Initialize the module
     def __init__(self, settings):
         self.context = None
         self.local_settings = settings
         self._logger = Logger.getLogger(self.__class__.__name__)
-        self._logger.log(Level.SEVERE, "Starting up plugin")
+        self._logger.log(Level.SEVERE, "Starting Forensics.im Plugin")
+        self.path_to_executable = None
+
+        communication_manager = Case.getCurrentCase().getSleuthkitCase().getCommunicationsManager()
+
+        self.account = CommunicationsManager.addAccountType(communication_manager, "Microsoft Teams", "Microsoft Teams")
 
     # Do some basic configuration at the startup of the module
     def startUp(self, context):
         self.context = context
         if PlatformUtil.isWindowsOS():
-            self.path_to_executable = os.path.join(os.path.dirname(os.path.abspath(__file__)), "parser.exe")
+            self.path_to_executable = os.path.join(os.path.dirname(os.path.abspath(__file__)), "main.exe")
             if not os.path.exists(self.path_to_executable):
-                raise IngestModuleException("Could not find parser.exe within the module directory.")
+                raise IngestModuleException("Could not find main.exe within the module directory.")
         else:
             raise IngestModuleException("This Plugin currently only works on Windows based systems.")
 
@@ -119,7 +125,8 @@ class ForensicIMIngestModule(DataSourceIngestModule):
             parent_path = content.getParentPath()
             parent_path = parent_path.replace("/", "\\")[1:]
 
-            temp_path_to_data_source = os.path.join(Case.getCurrentCase().getTempDirectory(), str(content.getDataSource().getId()))
+            temp_path_to_data_source = os.path.join(Case.getCurrentCase().getTempDirectory(),
+                                                    str(content.getDataSource().getId()))
             temp_path_to_parent = os.path.join(temp_path_to_data_source, parent_path)
             temp_path_to_content = os.path.join(temp_path_to_parent, content.getName())
             os.makedirs(temp_path_to_content)
@@ -135,109 +142,141 @@ class ForensicIMIngestModule(DataSourceIngestModule):
 
     def _extract(self, content, path):
         # This functions extracts the artefacts from the datasource
-        children = content.getChildren()
-        for child in children:
-            child_name = child.getName()
-            child_path = os.path.join(path, child_name)
-            # ignore relative paths
-            if child_name == "." or child_name == "..":
-                continue
-            elif child.isFile():
-                ContentUtils.writeToFile(child, File(child_path))
-            elif child.isDir():
-                os.mkdir(child_path)
-                self._extract(child, child_path)
-
+        try:
+            children = content.getChildren()
+            for child in children:
+                child_name = child.getName()
+                child_path = os.path.join(path, child_name)
+                # ignore relative paths
+                if child_name == "." or child_name == "..":
+                    continue
+                elif child.isFile():
+                    ContentUtils.writeToFile(child, File(child_path))
+                elif child.isDir():
+                    os.mkdir(child_path)
+                    self._extract(child, child_path)
+            self.log(Level.INFO, "Successfully extracted to {}".format(path))
+        except OSError:
+            raise IngestModuleException("Could not extract files to directory: {}.".format(path))
 
     def _analyze(self, content, path):
         # Piece together our command for running parse.exe with the appropriate parameters
-        self.log(Level.INFO, "Executing {} with file {}.".format(self.path_to_executable, path))
+        path_to_teams_json = os.path.join(path, "teams.json")
+        self.log(Level.INFO, "Executing {} with input path {} and output file {}.".format(self.path_to_executable, path,
+                                                                                          path_to_teams_json))
         cmd = ArrayList()
         cmd.add(self.path_to_executable)
         cmd.add("-f")
         cmd.add(path)
+        cmd.add("-o")
+        cmd.add(path_to_teams_json)
         process_builder = ProcessBuilder(cmd)
         ExecUtil.execute(process_builder, DataSourceIngestModuleProcessTerminator(self.context))
 
-        path_to_teams_json = os.path.join(path, "teams.json")
         if not os.path.exists(path_to_teams_json):
             raise IngestModuleException("Unable to find extracted data.")
 
+        # Lets attribute the messages to their respective source files
+        imported_records = []
+        with open(path_to_teams_json, "rb") as json_file:
+            imported_records = json.load(json_file)
+
+        database_sub_files = [i for n, i in enumerate(imported_records) if
+                              i.get('origin_file') not in [y.get('origin_file') for y in imported_records[n + 1:]]]
+
         sleuthkit_case = Case.getCurrentCase().getSleuthkitCase()
         module_name = ForensicIMIngestModuleFactory.moduleName
-        src_content = path
-        # TODO create custom account type or choose Skype http://sleuthkit.org/sleuthkit/docs/jni-docs/4.6/classorg_1_1sleuthkit_1_1datamodel_1_1_account_1_1_type.html
-        account_type = Account.Type.MESSAGING_APP
-        # TODO fix account name
-        self_account_id = "Jack the Ripper"
-        appDbHelper = CommunicationArtifactsHelper(sleuthkit_case, module_name, src_content, account_type, account_type, self_account_id)
+        # Process per file
+        for file in database_sub_files:
+            db_file_path = self.get_level_db_file(content, file['origin_file'])
+            helper = CommunicationArtifactsHelper(sleuthkit_case, module_name, db_file_path, Account.Type.MESSAGING_APP)
+            # Get only the records per file
+            file_entries = [d for d in imported_records if d['origin_file'] == file['origin_file']]
+            # get the messages
+            messages = [d for d in file_entries if d['type'] == 'message']
+            self.parse_messages(helper, messages)
+    def get_level_db_file(self, content, filepath):
+        # Get the file name
+        filename = str(filepath).split('\\')[-1:][0]
+        file_manager = Case.getCurrentCase().getServices().getFileManager()
+        data_source = content.getDataSource()
+        dir_name = os.path.join(content.getParentPath(), content.getName())
+        results = file_manager.findFiles(data_source, filename, dir_name)
+        if results.isEmpty():
+            self.log(Level.INFO, "Unable to locate {}".format(filename))
+            return
+        db_file = results.get(0)  # Expect a single match so retrieve the first (and only) file
+        return db_file
 
-        with open(path_to_teams_json, "rb") as json_file:
-            records = json.load(json_file)
-            for record in records:
-                if record['type'] == 'message':
-                    self._newArtifactTSKMessage(appDbHelper, record['content'])
-                # TODO implement other record types (calls, appointments etc.)
-    def _newArtifactTSKMessage(self, app_db_helper, message):
+
+    def parse_messages(self, app_db_helper, messages):
         # We will use the integrated add message function to add a TSK_MESSAGE
         # http://sleuthkit.org/autopsy/docs/api-docs/4.12.0//classorg_1_1sleuthkit_1_1autopsy_1_1coreutils_1_1_app_d_b_parser_helper.html#a630bb70bee171df941e363693a1795f3
+        for message in messages:
+            message_type = "Microsoft Teams (Direct Message)"
+            # TODO Change back an email address
+            from_address = message["imdisplayname"]
+            to_address = ""
+            subject = ""
+            message_text = message["content"]
+            # Timestamp
+            dt = datetime.strptime(message['composetime'][:19], "%Y-%m-%dT%H:%M:%S")
+            time_struct = dt.timetuple()
+            timestamp = int(calendar.timegm(time_struct))
+            # Group by the conversationId, these can be direct messages, but also posts
+            thread_id = message["conversationId"]
+            # TODO Fix direction, possibly determine direction based on account name?
+            direction = CommunicationDirection.UNKNOWN
 
-        message_type = "Microsoft Teams (Direct Message)"
-        from_address = message["userPrincipalName"]
-        to_address = ""
-        subject = ""
-        message_text = message["content"]
-        # Timestamp
-        dt = datetime.strptime(message['composetime'][:19], "%Y-%m-%dT%H:%M:%S")
-        time_struct = dt.timetuple()
-        timestamp = int(calendar.timegm(time_struct))
-        # Group by the conversationId, these can be direct messages, but also posts
-        thread_id = message["conversationId"]
-        # TODO Fix direction
-        direction = CommunicationDirection.UNKNOWN
+            # Create the actual artefact in blackboard
+            artifact = app_db_helper.addMessage(message_type, direction, from_address, to_address, timestamp,
+                                                MessageReadStatus.UNKNOWN, subject, message_text, thread_id)
 
-        # Create the actual artefact in blackboard
-        artifact = app_db_helper.addMessage(message_type, direction, from_address, to_address, timestamp, MessageReadStatus.UNKNOWN, subject, message_text, thread_id)
+            # TODO add message attachments URLs and Media Files
 
-        # TODO add message attachments URLs and Media Files
 
-        return  artifact
 
     def process(self, data_source, progress_bar):
 
-            # we don't know how long it takes
-            progress_bar.switchToIndeterminate()
+        # we don't know how long it takes
+        progress_bar.switchToIndeterminate()
 
-            # Locate the leveldb database. The full path on Windows systems is something like
-            # C:\Users\<user>\AppData\Roaming\Microsoft\Teams\IndexedDB\https_teams.microsoft.com_0.indexeddb.leveldb
+        # Locate the leveldb database. The full path on Windows systems is something like
+        # C:\Users\<user>\AppData\Roaming\Microsoft\Teams\IndexedDB\https_teams.microsoft.com_0.indexeddb.leveldb
 
-            file_manager = Case.getCurrentCase().getServices().getFileManager()
-            directory = "https_teams.microsoft.com_0.indexeddb.leveldb"
-            parent_directory = "AppData/Roaming/Microsoft/Teams/IndexedDB"
-            all_ms_teams_leveldbs = file_manager.findFiles(data_source, directory, parent_directory)
+        file_manager = Case.getCurrentCase().getServices().getFileManager()
+        directory = "https_teams.microsoft.com_0.indexeddb.leveldb"
+        # AppData/Roaming/Microsoft/Teams/IndexedDB
+        parent_directory = "IndexedDB"
+        all_ms_teams_leveldbs = file_manager.findFiles(data_source, directory, parent_directory)
 
-            # Loop over all the files. On a multi user account these could be multiple one.
-            directories_to_process = len(all_ms_teams_leveldbs)
-            progress_bar.switchToDeterminate(directories_to_process)
+        # Loop over all the files. On a multi user account these could be multiple one.
+        directories_to_process = len(all_ms_teams_leveldbs)
 
-            for i, content in enumerate(all_ms_teams_leveldbs):
-                # Check if the user pressed cancel while we are processing the files
-                if self.context.isJobCancelled():
-                    message = IngestMessage.createMessage(IngestMessage.MessageType.WARNING, ForensicIMIngestModuleFactory.moduleName, "Analysis of LevelDB has been aborted.")
-                    IngestServices.getInstance().postMessage(message)
-                    return IngestModule.ProcessResult.OK
-                # Update progress both to the progress bar and by loggging
-                progress_bar.progress(i)
-                self.log(Level.INFO, "Processing item {} of {}: {}".format(i, directories_to_process, content.getName()))
-                # Ignore false positives
-                if not content.isDir():
-                    continue
-                # Where the REAL extraction and analysis happens
-                self._parse_databases(content)
+        self.log(Level.INFO, "Found {} Microsoft Teams directories to process.".format(directories_to_process))
+        progress_bar.switchToDeterminate(directories_to_process)
 
-            # Once we are done, post a message to the ingest messages box
-            # Message type DATA seems most appropriate
-            # https://www.sleuthkit.org/autopsy/docs/api-docs/4.0/enumorg_1_1sleuthkit_1_1autopsy_1_1ingest_1_1_ingest_message_1_1_message_type.html
-            message = IngestMessage.createMessage(IngestMessage.MessageType.DATA, ForensicIMIngestModuleFactory.moduleName, "Finished analysing the LeveLDB from Microsoft Teams.")
-            IngestServices.getInstance().postMessage(message)
-            return IngestModule.ProcessResult.OK
+        for i, content in enumerate(all_ms_teams_leveldbs):
+            # Check if the user pressed cancel while we are processing the files
+            if self.context.isJobCancelled():
+                message = IngestMessage.createMessage(IngestMessage.MessageType.WARNING,
+                                                      ForensicIMIngestModuleFactory.moduleName,
+                                                      "Analysis of LevelDB has been aborted.")
+                IngestServices.getInstance().postMessage(message)
+                return IngestModule.ProcessResult.OK
+            # Update progress both to the progress bar and by loggging
+            progress_bar.progress(i)
+            self.log(Level.INFO, "Processing item {} of {}: {}".format(i, directories_to_process, content.getName()))
+            # Ignore false positives
+            if not content.isDir():
+                continue
+            # Where the REAL extraction and analysis happens
+            self._parse_databases(content)
+
+        # Once we are done, post a message to the ingest messages box
+        # Message type DATA seems most appropriate
+        # https://www.sleuthkit.org/autopsy/docs/api-docs/4.0/enumorg_1_1sleuthkit_1_1autopsy_1_1ingest_1_1_ingest_message_1_1_message_type.html
+        message = IngestMessage.createMessage(IngestMessage.MessageType.DATA, ForensicIMIngestModuleFactory.moduleName,
+                                              "Finished analysing the LeveLDB from Microsoft Teams.")
+        IngestServices.getInstance().postMessage(message)
+        return IngestModule.ProcessResult.OK
